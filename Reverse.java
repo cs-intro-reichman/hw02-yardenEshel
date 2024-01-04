5237faa74b5ddab863630468c1a103b723eb42f9
Yarden Eshel <yardenyarden5@gmail.com>
2024-01-04 15:19:53 +0200

    reverse.java updated

--- Reverse.java
+++ Reverse.java
@@ -3,12 +3,20 @@ class Reverse {
     public static void main(String args[])
     {
         String x = args[0];
-
+        int len = 0;
         for(int i = x.length() - 1; i >= 0; i --)
         {
             System.out.print(x.charAt(i));
 
         }
-        System.out.println("\nThe middle character is " + x.charAt(x.length()/2));
+        if(x.length()%2 == 0)
+        {
+            len = x.length()/2 - 1;
+        }
+        else
+        {
+            len = x.length()/2;
+        }
+        System.out.println("\nThe middle character is " + x.charAt(len));
     }
 }
